9dad114542a61dc38f73a9ac75d184726067d1cc
safna <fathimathu29@gmail.com>
2025-11-26 22:44:45 +0530

    change in routes request

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -3,16 +3,18 @@ import {bloodRequest,approveRespond,acceptBloodRequest,getAllAcceptedRequests,re
 import protect from '../middleWare/userMiddleWare.js';
 
 const app = express.Router()
-app.route('/').post(protect,bloodRequest).get(getAllBloodRequest)
-app.route('/accept').post(protect,acceptBloodRequest).get(protect,getAllAcceptedRequests)
-app.route('/:id').get(getBloodRequest)
-app.route('/reject').post(protect,rejectBloodRequest)
-app.route('/approve/:id').post(protect,approveRespond)
-app.route('/accept/:id').post(protect,acceptBloodRequest)
+app.route('/').post(protect, bloodRequest).get(getAllBloodRequest);
+app.route('/accept').post(protect, acceptBloodRequest).get(protect, getAllAcceptedRequests);
+app.route('/reject').post(protect, rejectBloodRequest);
+app.route('/approve/:id').post(protect, approveRespond);
+app.route('/accept/:id').post(protect, acceptBloodRequest);
 
-//admin
-app.route("/history/:id").get(getUserById)
-//user history
-app.route("/history").get(protect,getHistory)
+// MOVE THIS TO LAST
+app.route('/:id').get(getBloodRequest);
+
+// Admin
+app.route("/history/:id").get(getUserById);
+app.route("/history").get(protect, getHistory);
+
 
 export default app
