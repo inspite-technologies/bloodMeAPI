6459b461078435e5f0d273e3041a1b365b74bdee
safna <fathimathu29@gmail.com>
2025-11-27 13:25:31 +0530

    token middleware

--- middleWare/updateFcmToken.js
+++ middleWare/updateFcmToken.js
@@ -1,10 +1,16 @@
+import User from "../models/userModel.js";
+
 export const updateFcmToken = async (req, res, next) => {
   try {
     const userId = req.user?._id;
-    const fcmToken = req.headers['fcm-token'];
+    const fcmToken = req.headers["fcm-token"];
 
     if (userId && fcmToken) {
-      await User.findByIdAndUpdate(userId, { fcmToken }, { new: true });
+      await User.findByIdAndUpdate(
+        userId,
+        { fcmToken },
+        { new: true }
+      );
       console.log("FCM Token updated for user:", userId);
     }
 
